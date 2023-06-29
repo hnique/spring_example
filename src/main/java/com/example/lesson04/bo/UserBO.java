2092894cfae39a7a72f72a145460a90e987dcc52
test <test@test.com>
2023-06-29 18:41:59 +0900

    0629 수업

--- src/main/java/com/example/lesson04/bo/UserBO.java
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -18,4 +18,8 @@ public class UserBO {
 	public User getLatestUser() {
 		return userMapper.selectLatestUser();
 	}
+	
+	public boolean existUserByName(String name) {
+		return userMapper.existUserByName(name);
+	}
 }
